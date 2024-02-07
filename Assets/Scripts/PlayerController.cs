620f78f74c9d77100b61d3d3a0c9440772ef7166
connortrue <connor.true.94@gmail.com>
2024-02-06 18:38:43 -0600

    4, 78 coins, all under the heirchy. I have a plan.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,12 @@
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody player;
+    public Rigidbody coin;
 
     public float move = 5000f;
+    public int score = 0;
     void FixedUpdate()
     {
         if (Input.GetKey("d"))
@@ -23,6 +24,16 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey("s"))
         {
             player.AddForce(0,0, -move * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            score+=100;
+            Debug.Log("Score: " + score);
+            Destroy(other.gameObject);
         }
     }
 }
